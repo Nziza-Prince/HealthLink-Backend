647c395842c0510b460cfe6ae01d3a4f1a4ba652
Nziza-Prince <nzizaprince7@gmail.com>
2025-08-21 23:23:06 +0200

    initial commit

---
+++ src/main/java/com/healthlinkteam/healthlink/HealthlinkApplication.java
@@ -0,0 +1,11 @@
+package com.healthlinkteam.healthlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthlinkApplication {
+    public static void main(String[] args) {
+		SpringApplication.run(HealthlinkApplication.class, args);
+	}
+}
---
+++ src/test/java/com/healthlinkteam/healthlink/HealthlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthlinkteam.healthlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthlinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
